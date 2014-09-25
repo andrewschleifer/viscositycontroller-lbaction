7099020e04aca6ad137cca878179fb78fe738207
Andrew Schleifer <me@andrewschleifer.name>
2014-09-25 16:16:40 -0500

    dig out name of remote host and display

--- Contents/Scripts/default.sh
+++ Contents/Scripts/default.sh
@@ -5,21 +5,23 @@ echo '['
 
 osascript -e "tell application \"Viscosity\" to get the name of every connection whose state is \"Connected\"" | \
     while read NAME; do
+        HOST=$(grep remote "$(grep -rl viscosity\ name\ Anchor ~/Library/Application\ Support/Viscosity/OpenVPN/)" | cut -d\  -f2)
         echo $NAME | \
         sed '
             /^\s*$/d;
             s|^|{ "title": "|;
-            s|$|", "subtitle": "Disconnect", "icon": "com.viscosityvpn.Viscosity", "action": "disconnect.sh", "actionArgument": "'$NAME'" },|
+            s|$|", "subtitle": "Disconnect from '$HOST'", "icon": "com.viscosityvpn.Viscosity", "action": "disconnect.sh", "actionArgument": "'$NAME'" },|
         '
     done
 
 osascript -e "tell application \"Viscosity\" to get the name of every connection whose state is not \"Connected\"" | \
     while read NAME; do
+        HOST=$(grep remote "$(grep -rl viscosity\ name\ Anchor ~/Library/Application\ Support/Viscosity/OpenVPN/)" | cut -d\  -f2)
         echo $NAME | \
         sed '
             /^\s*$/d;
             s|^|{ "title": "|;
-            s|$|", "subtitle": "Connect", "icon": "com.viscosityvpn.Viscosity", "action": "connect.sh", "actionArgument": "'$NAME'" },|
+            s|$|", "subtitle": "Connect to '$HOST'", "icon": "com.viscosityvpn.Viscosity", "action": "connect.sh", "actionArgument": "'$NAME'" },|
         '
     done
 
