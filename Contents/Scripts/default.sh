4502e26bb16968d2e67878c936d2ef13d3507ce2
Andrew Schleifer <me@andrewschleifer.name>
2015-02-22 23:45:02 -0600

    update for new versions
    * viscosity output changed
    * yosemite has a new sed

--- Contents/Scripts/default.sh
+++ Contents/Scripts/default.sh
@@ -4,25 +4,23 @@
 echo '['
 
 osascript -e "tell application \"Viscosity\" to get the name of every connection whose state is \"Connected\"" | \
+    tr -d '[:blank:]'  | tr ',' '\012' | sed '/^[[:space:]]*$/d'| \
     while read NAME; do
-        HOST=$(grep remote "$(grep -rl viscosity\ name\ Anchor ~/Library/Application\ Support/Viscosity/OpenVPN/)" | cut -d\  -f2)
+        HOST=$(find ~/Library/Application\ Support/Viscosity/OpenVPN -name config.conf -print0 | xargs -0 grep remote\ $NAME | cut -d\  -f3)
         echo $NAME | \
-        sed '
-            /^\s*$/d;
-            s|^|{ "title": "|;
-            s|$|", "subtitle": "Disconnect from '$HOST'", "icon": "com.viscosityvpn.Viscosity", "action": "disconnect.sh", "actionArgument": "'$NAME'" },|
-        '
+        sed \
+            -e 's|^|{ "title": "|' \
+            -e 's|$|", "subtitle": "Disconnect from '$HOST'", "icon": "com.viscosityvpn.Viscosity", "action": "disconnect.sh", "actionArgument": "'$NAME'" },|'
     done
 
 osascript -e "tell application \"Viscosity\" to get the name of every connection whose state is not \"Connected\"" | \
+    tr -d '[:blank:]'  | tr ',' '\012' | sed '/^[[:space:]]*$/d'| \
     while read NAME; do
-        HOST=$(grep remote "$(grep -rl viscosity\ name\ Anchor ~/Library/Application\ Support/Viscosity/OpenVPN/)" | cut -d\  -f2)
+        HOST=$(find ~/Library/Application\ Support/Viscosity/OpenVPN -name config.conf -print0 | xargs -0 grep remote\ $NAME | cut -d\  -f3)
         echo $NAME | \
-        sed '
-            /^\s*$/d;
-            s|^|{ "title": "|;
-            s|$|", "subtitle": "Connect to '$HOST'", "icon": "com.viscosityvpn.Viscosity", "action": "connect.sh", "actionArgument": "'$NAME'" },|
-        '
+        sed \
+            -e 's|^|{ "title": "|' \
+            -e 's|$|", "subtitle": "Connect to '$HOST'", "icon": "com.viscosityvpn.Viscosity", "action": "connect.sh", "actionArgument": "'$NAME'" },|'
     done
 
 echo ']'
